91df7bd7ab60281ffef11592db7459d08b6f69e1
Pham Van Hieu <mr_hie@yahoo.com>
2019-07-10 16:07:19 +0800

    feat: correct configuration for balloon block toolbar

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,26 +2,21 @@
 * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 *
+* See more: https://github.com/ckeditor/ckeditor5-build-balloon-block/blob/master/src/ckeditor.js
 * Customized by hieu@creadits.com
 */
 
 // The editor creator to use.
 import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-// import InlineEditorBase from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
 import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
-// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-// import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily';
-// import FontSize from '@ckeditor/ckeditor5-font/src/fontsize';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-// import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import Image from '@ckeditor/ckeditor5-image/src/image';
-// import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
 import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
 import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
@@ -31,7 +26,6 @@ import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
-// import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
@@ -43,8 +37,6 @@ import CommentsOnly from '@ckeditor/ckeditor5-comments/src/commentsonly';
 import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
 
 class ClassicEditor extends ClassicEditorBase {}
-// class InlineEditor extends InlineEditorBase {}
-// class InlineEditorWithComment extends InlineEditorBase {}
 class BalloonEditor extends BalloonEditorBase {}
 class BalloonEditorWithComment extends BalloonEditorBase {}
 
@@ -52,17 +44,12 @@ class BalloonEditorWithComment extends BalloonEditorBase {}
 const builtinPlugins = [
 	Alignment,
 	Autoformat,
-	// BlockQuote,
 	Bold,
 	CKFinder,
 	EasyImage,
 	Essentials,
-	// FontFamily,
-	// FontSize,
 	Heading,
-	// Highlight,
 	Image,
-	// ImageCaption,
 	ImageStyle,
 	ImageToolbar,
 	ImageUpload,
@@ -72,7 +59,6 @@ const builtinPlugins = [
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
-	// RemoveFormat,
 	Strikethrough,
 	Table,
 	TableToolbar,
@@ -82,15 +68,6 @@ const builtinPlugins = [
 ];
 
 ClassicEditor.builtinPlugins = builtinPlugins;
-
-// InlineEditor.builtinPlugins = builtinPlugins;
-//
-// InlineEditorWithComment.builtinPlugins = [
-// 	...builtinPlugins,
-// 	BalloonToolbar,
-// 	Comments,
-// 	CommentsOnly,
-// ];
 
 BalloonEditor.builtinPlugins = builtinPlugins;
 
@@ -103,33 +80,16 @@ BalloonEditorWithComment.builtinPlugins = [
 
 // Editor configuration.
 const defaultConfig = {
+	blockToolbar: [
+		'link', 'bulletedList', 'numberedList', 'alignment',
+		'|',
+		'insertTable', 'imageUpload', 'mediaEmbed',
+	],
 	toolbar: {
 		items: [
 			'heading',
-			// '|',
-			// 'fontsize',
-			// 'fontfamily',
 			'|',
-			'bold',
-			'italic',
-			'underline',
-			'strikethrough',
-			// 'removeFormat',
-			// 'highlight',
-			'|',
-			'alignment',
-			'|',
-			// 'numberedList',
-			// 'bulletedList',
-			// '|',
-			'link',
-			// 'blockquote',
-			'insertTable',
-			'imageUpload',
-			'mediaEmbed',
-			'|',
-			'undo',
-			'redo',
+			'bold', 'italic', 'underline', 'strikethrough',
 		]
 	},
 	image: {
@@ -137,8 +97,6 @@ const defaultConfig = {
 			'imageStyle:alignLeft',
 			'imageStyle:full',
 			'imageStyle:alignRight',
-			// '|',
-			// 'imageTextAlternative',
 		],
 		styles: [
 			'alignLeft',
@@ -153,13 +111,13 @@ const defaultConfig = {
 			'mergeTableCells'
 		]
 	},
-	blockToolbar: [ 'heading', '|', 'bulletedList', 'numberedList' ],
+	link: {
+		addTargetToExternalLinks: true
+	},
 	language: 'en'
 };
 
 ClassicEditor.defaultConfig = defaultConfig;
-
-// InlineEditor.defaultConfig = defaultConfig;
 
 BalloonEditor.defaultConfig = defaultConfig;
 
@@ -175,8 +133,6 @@ defaultConfigWithComment.image = {
 		'imageStyle:alignLeft',
 		'imageStyle:full',
 		'imageStyle:alignRight',
-		'|',
-		'imageTextAlternative',
 		'|',
 		'comment'
 	],
@@ -209,8 +165,6 @@ BalloonEditorWithComment.defaultConfig = defaultConfigWithComment;
 
 export default {
 	ClassicEditor,
-	// InlineEditor,
-	// InlineEditorWithComment,
 	BalloonEditor,
 	BalloonEditorWithComment,
 };
